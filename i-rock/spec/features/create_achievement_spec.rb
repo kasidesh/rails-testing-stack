4e795b3f5db538cc4c4f8c1907d18e1a5f5c49da
Kasidesh Yontaradidthaworn <kasidesh@gmail.com>
2016-12-19 01:15:37 +0700

    achievement form

--- i-rock/spec/features/create_achievement_spec.rb
+++ i-rock/spec/features/create_achievement_spec.rb
@@ -4,12 +4,11 @@ feature 'create new achievement' do
   scenario 'create new achievement with valid data' do
     visit('/')
     click_on('New Achievement')
-
     fill_in('Title', with: 'Read a book')
     fill_in('Description', with: 'Excellent read')
     select('Public', from: 'Privacy')
     check('Featured achievement')
-    atach_file('Cover image' , "#{Rails.root}/spec/features/cover_image.png")
+    attach_file('Cover image' , "#{Rails.root}/spec/fixtures/cover_image.png")
     click_on('Create Achievement')
 
     expect(page).to have_content('Achievement has been created')
